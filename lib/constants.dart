fb13c5db63db9ede4b087386f7ee044d3d4d73e9
Anas Syed <123989080+anasyd@users.noreply.github.com>
2024-03-14 15:07:16 +0000

    Add noImage from API error message

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,4 +2,5 @@ class MessageConstants {
   static const errorMessage =
       "Error Connecting!\nPlease Check your\nnetwork connection.";
   static const imageErrorMessage = "Failed to load image.";
+  static const noImageErrorMessage = "No Image.";
 }
