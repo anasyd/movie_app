0835876af73c27e1ef97c77b29bd8669db89ae8b
Anas Syed <123989080+anasyd@users.noreply.github.com>
2024-03-14 16:44:43 +0000

    remove unused code

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -30,31 +30,41 @@ class MovieApiImpl extends MovieApi {
   static const _lastUpdateKeyTrending = 'last_trending_update';
   static const _lastUpdateKeyTopRated = 'last_top_rated_update';
 
-  // DateTime? _lastUpdateTrending;
-  // DateTime? _lastUpdateTopRated;
-
+  // Function to save the last update timestamp for a given key to Firestore
   Future<void> _saveLastUpdate(String key) async {
+    // Get current timestamp
     final timestamp = DateTime.now();
     await FirebaseFirestore.instance
+        // Access the 'app_settings' collection
         .collection('app_settings')
+
+        // Get document with the provided key
         .doc(key)
+
+        // Set the 'timestamp' field to the current timestamp
         .set({'timestamp': timestamp});
-    // if (key == _lastUpdateKeyTrending) {
-    //   _lastUpdateTrending = timestamp;
-    // } else if (key == _lastUpdateKeyTopRated) {
-    //   _lastUpdateTopRated = timestamp;
-    // }
   }
 
+// Function to retrieve the last update timestamp for a given key from Firestore
   Future<DateTime?> _getLastUpdate(String key) async {
     final snapshot = await FirebaseFirestore.instance
+        // Access the 'app_settings' collection
         .collection('app_settings')
+
+        // Get document with the provided key
         .doc(key)
+
+        // Get document snapshot
         .get();
     if (snapshot.exists) {
+      // Retrieve timestamp from snapshot data
       final timestamp = snapshot.data()?['timestamp'] as Timestamp?;
+
+      // Convert Firestore Timestamp to DateTime and return
       return timestamp?.toDate();
     }
+
+    // Return null if the document does not exist
     return null;
   }
 
@@ -96,9 +106,16 @@ class MovieApiImpl extends MovieApi {
   @override
   Future<List<Cast>> getCastList(int id) async {
     try {
+      // Get cast list for a movie from the API
       final response = await _client.get('movie/$id/credits');
+
+      // Extract cast data from the response
       final data = response['cast'] as List;
+
+      // Map the cast data to Cast objects
       final cast = data.map((cast) => Cast.fromJson(cast)).toList();
+
+      // Return the cast list
       return cast;
     } on SocketException {
       // No network exception
@@ -112,10 +129,17 @@ class MovieApiImpl extends MovieApi {
   @override
   Future<List<Movie>> getSearchResults(String query) async {
     try {
+      // Search for movies with the provided query
       final response =
           await _client.get(_searchUrlPath, params: {'query': query});
+
+      // Extract movie search results from the response
       final data = response['results'] as List;
+
+      // Map the movie data to Movie objects
       final movies = data.map((movie) => Movie.fromJson(movie)).toList();
+
+      // Return the list of search results
       return movies;
     } on SocketException {
       // No network exception
@@ -129,19 +153,28 @@ class MovieApiImpl extends MovieApi {
   @override
   Future<List<Movie>> getTrendingMovies() async {
     try {
+      // Get the last update timestamp for trending movies
       final lastUpdate = await _getLastUpdate(_lastUpdateKeyTrending);
+
+      // Get the current timestamp
       final now = DateTime.now();
 
+      // Check if the data needs to be updated (either it's the first update or more than a day has passed)
       if (lastUpdate == null || now.difference(lastUpdate).inDays >= 1) {
+        // Fetch and update movies from the API
         await _fetchAndUpdateMovies(
             _trendingUrlPath, _firebaseTrending, _lastUpdateKeyTrending);
+
+        // Save the last update timestamp
         await _saveLastUpdate(_lastUpdateKeyTrending);
       }
 
+      // Get trending movies from the API
       final response = await _client.get(_trendingUrlPath);
       final data = response['results'] as List;
       final movies = data.map((movie) => Movie.fromJson(movie)).toList();
 
+      // Return the list of trending movies
       return movies;
     } on SocketException {
       throw const SocketException("No network");
@@ -152,6 +185,8 @@ class MovieApiImpl extends MovieApi {
           await FirebaseFirestore.instance.collection(_firebaseTrending).get();
       final movies =
           snapshot.docs.map((doc) => Movie.fromJson(doc.data())).toList();
+
+      // Return the list of trending movies from Firebase
       return movies;
     }
   }
@@ -159,15 +194,23 @@ class MovieApiImpl extends MovieApi {
   @override
   Future<List<Movie>> getTopRatedMovies() async {
     try {
+      // Get the last update timestamp for topRated movies
       final lastUpdate = await _getLastUpdate(_lastUpdateKeyTopRated);
+
+      // Get the current timestamp
       final now = DateTime.now();
 
+      // Check if the data needs to be updated (either it's the first update or more than a day has passed)
       if (lastUpdate == null || now.difference(lastUpdate).inDays >= 1) {
+        // Fetch and update movies from the API
         await _fetchAndUpdateMovies(
             _topRatedUrlPath, _firebaseTopRated, _lastUpdateKeyTopRated);
+
+        // Save the last update timestamp
         await _saveLastUpdate(_lastUpdateKeyTopRated);
       }
 
+      // Get topRated movies from the API
       final response = await _client.get(_topRatedUrlPath);
       final data = response['results'] as List;
       final movies = data.map((movie) => Movie.fromJson(movie)).toList();
@@ -181,6 +224,8 @@ class MovieApiImpl extends MovieApi {
           await FirebaseFirestore.instance.collection(_firebaseTopRated).get();
       final movies =
           snapshot.docs.map((doc) => Movie.fromJson(doc.data())).toList();
+
+      // Return the list of topRated movies from Firebase
       return movies;
     }
   }
